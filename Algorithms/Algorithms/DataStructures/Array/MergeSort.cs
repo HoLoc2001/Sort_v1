2cfce6de7759594c51c60f8e705aea920d01ab1f
HoLoc2001 <hotanloc2001@gmail.com>
2021-10-31 12:25:45 +0700

    update

--- Algorithms/Algorithms/DataStructures/Array/MergeSort.cs
+++ Algorithms/Algorithms/DataStructures/Array/MergeSort.cs
@@ -12,6 +12,62 @@ namespace Algorithms.DataStructures.Array
 {
     public class MergeSort
     {
+        public static void merge(int[] arr, int l, int m, int r)
+        {
+            int n1 = m - l + 1;
+            int n2 = r - m;
 
+            int[] L = new int[n1];
+            int[] R = new int[n2];
+            int i, j;
+            for (i = 0; i < n1; ++i)
+                L[i] = arr[l + i];
+            for (j = 0; j < n2; ++j)
+                R[j] = arr[m + 1 + j];
+
+            i = 0;
+            j = 0;
+
+            int k = l;
+            while (i < n1 && j < n2)
+            {
+                if (L[i] <= R[j])
+                {
+                    arr[k] = L[i];
+                    i++;
+                }
+                else
+                {
+                    arr[k] = R[j];
+                    j++;
+                }
+                k++;
+            }
+
+            while (i < n1)
+            {
+                arr[k] = L[i];
+                i++;
+                k++;
+            }
+            while (j < n2)
+            {
+                arr[k] = R[j];
+                j++;
+                k++;
+            }
+        }
+        void Merge_Sort(int[] arr, int l, int r) //l = 0, r = length -1
+        {
+            if (l < r)
+            {
+                int m = l + (r - l) / 2;
+
+                Merge_Sort(arr, l, m);
+                Merge_Sort(arr, m + 1, r);
+
+                merge(arr, l, m, r);
+            }
+        }
     }
 }
